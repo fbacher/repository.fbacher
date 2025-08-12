54a13eb49ba22218c61d6c33040bce3710e97675
Frank Feuerbacher <frank@feuerbacher.us>
2025-08-12 18:40:16 -0500

    Provide xml and xml.gz

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 cd repo/zips
 rm -rf *
+python3 ../../create_repository.py ../repository.fbacher ../script.module.langcodes ../service.kodi.tts
 python3 ../../create_repository.py --compressed ../repository.fbacher ../script.module.langcodes ../service.kodi.tts
